7edb2ab53f0d476711352f753d184cd5aceb0bfb
James Disusa <james.disusa123@gmail.com>
2026-01-24 00:10:01 +0530

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -18,8 +18,9 @@ let drops = new Array(columns).fill(1);
 
 function draw() {
 
-  ctx.fillStyle = "rgba(34,211,238,0.8)";
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 
   ctx.fillStyle = "#22d3ee";
   ctx.font = fontSize + "px monospace";
